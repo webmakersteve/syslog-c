71f5b0b6f8a839f0f3cd4f71fab1835519f2cccc
Stephen Parente <webmakersteve@gmail.com>
2017-02-21 00:07:27 -0800

    Added little benchmark

--- tests/test.c
+++ tests/test.c
@@ -1,4 +1,5 @@
-# include "syslog.h"
+#include "syslog.h"
+#include "time.h"
 
 int main(int argc, char** argv) {
 	char * mm = "<165>1 2016-12-16T12:00:00.000Z hostname appname PROCID MSGID [exampleSDID@32473 eventSource=\"Application\" eventID=\"1011\"] Logging message...";
@@ -7,6 +8,25 @@ int main(int argc, char** argv) {
 	if (!parse_syslog(mm, &msg)) {
 		return 1;
 	}
+
+	float start_time = (float)clock()/CLOCKS_PER_SEC;
+	int num_messages = 180000;
+
+	for (int i = 0; i < num_messages; i++) {
+		syslog_message_t m = {};
+		if (!parse_syslog(mm, &m)) {
+			return 1;
+		}
+		free_syslog_message_t(&m);
+	}
+
+	/* Do work */
+
+	float end_time = (float)clock()/CLOCKS_PER_SEC;
+
+	float time_elapsed = end_time - start_time;
+
+	printf("Took %f to process %d messages\n\n", time_elapsed, num_messages);
 
 	printf("Severity: %d\n", msg.severity);
 	printf("Facility: %d\n", msg.facility);
